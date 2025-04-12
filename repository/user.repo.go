823a2ecc1dd549ee577973633c93c7778aba4660
Djuanzz <adnanjuan06@gmail.com>
2025-04-12 19:40:54 +0700

    fix: is nrp exist logic

--- repository/user.repo.go
+++ repository/user.repo.go
@@ -59,7 +59,7 @@ func (r *userRepository) IsNRPExist(nrp string) (bool, error) {
 	var user model.User
 	if err := r.DB.Where("nrp = ?", nrp).Take(&user).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
-			return false, err
+			return false, nil
 		}
 		return false, err
 	}
